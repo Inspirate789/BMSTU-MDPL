c1cb82b7eb6d391a168b481fe3bc83c95465ca07
Inspirate789 <andreysapozhkov535@gmail.com>
2022-04-04 17:12:08 +0300

    Add lab_06 and readme for lab_05

---
+++ lab_06/input_unsign_oct.asm
@@ -0,0 +1,72 @@
+PUBLIC input_unsigned_oct
+PUBLIC Number
+
+DSEG SEGMENT PARA PUBLIC 'DATA'
+	InputMSG            db "Input unsigned octal number: ", '$'
+    NumberErrorMSG      db "Incorrect number. Please, try again.", '$'
+	Number				dw 0                                            ; число занимает 16 бит, старший бит может рассматриваться как знаковый при выводе в знаковом представлении
+	Endline             db 13, 10, '$'
+DSEG ENDS
+
+CSEG2 SEGMENT PARA PUBLIC 'CODE'
+	assume CS:CSEG2, DS:DSEG                ; сегменты данных написаны так, чтобы они все объединялись в один и не нужно было в каждом модуле загружать свои сегменты в DS (именно в DS, потому что 09h работает с парой регистров ds:dx)
+
+input_unsigned_oct proc far
+	mov dx, OFFSET InputMSG
+    mov ah, 09h
+    int 21h                                 ; выводим приглашение к вводу
+
+    mov dx, OFFSET Endline
+	int 21h
+
+    xor bx, bx                              ; зануляем регистр, в который мы будем вводить наше число
+
+    mov cx, 5                               ; в 16 бит влезет максимум 5 восьмеричных цифр (по 3 бита каждая)
+    mov ah, 01h                             ; выставляем функцию ввода символа
+    
+    input_digits_loop:
+        int 21h                             ; вводим символ
+
+        cmp al, 13
+        je exit_success                     ; нажатие enter является признаком окончания ввода
+
+        sub al, '0'                         ; вычитаем '0', чтобы получить из кода символа само число (цифру)
+
+        cmp al, 7
+        ja exit_failure                     ; если цифра не восьмеричная, выходим, печатая сообщение об ошибке
+
+        insert_digit:
+        mov dx, cx                          ; сохраняем значение счётчика цикла в регистр dx
+        mov cl, 3
+        shl bx, cl                          ; сдвигаем число на 3 двоичных разряда влево
+        mov cx, dx                          ; восстанавливаем значение счётчика цикла из регистра dx
+
+        add bl, al                          ; помещаем введённную цифру в конец числа
+            
+        loop input_digits_loop
+
+    int 21h                                 ; после 5 цифр ожидаем enter
+    cmp al, 13
+    jne exit_failure
+    
+    exit_success:
+    mov Number, bx                          ; помещаем полученное число в Number только при полностью успешном вводе
+    ret
+    
+    exit_failure:
+    mov dx, OFFSET Endline
+	mov ah, 09h
+	int 21h
+
+	mov dx, OFFSET NumberErrorMSG           ; выводим сообщение об ошибке
+	int 21h
+
+    mov dx, OFFSET Endline
+	int 21h
+    
+    ret
+     
+input_unsigned_oct endp
+
+CSEG2 ENDS
+END
---
+++ lab_06/output_unsign_bin.asm
@@ -0,0 +1,44 @@
+EXTRN Number: word
+
+PUBLIC output_unsign_bin
+
+DSEG SEGMENT PARA PUBLIC 'DATA'
+	OutputBinMSG        db "Unsigned binary number representation: ", '$'
+	Endline             db 13, 10, '$'
+DSEG ENDS
+
+CSEG3 SEGMENT PARA PUBLIC 'CODE'
+	assume CS:CSEG3, DS:DSEG
+
+output_unsign_bin proc far
+	mov dx, OFFSET OutputBinMSG         ; выводим подсказку к выводу числа
+	mov ah, 09h
+	int 21h
+
+    mov dx, OFFSET Endline
+	int 21h
+
+    mov bx, Number                      ; помещаем наше число в регистр bx
+    
+    mov cx, 16                          ; будем выводить 16 двоичных цифр
+    mov ah, 02h                         ; выставляем функцию вывода символа
+
+    output_digits_loop:
+        mov dl, 0
+
+        sal bx, 1                       ; сдвигаем bx на 1 разряд влево через флаг CF
+        adc dl, '0'                     ; в зависимости от флага CF получаем в регистре dl код '0' или '1'
+
+        int 21h                         ; выводим символ
+
+        loop output_digits_loop
+    
+	mov dx, OFFSET Endline
+	mov ah, 09h
+	int 21h
+
+    ret
+output_unsign_bin endp
+
+CSEG3 ENDS
+END
