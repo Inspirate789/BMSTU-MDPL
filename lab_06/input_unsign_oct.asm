93383d054cb7d1fea5ffd7721723c007ce8e08ff
Inspirate789 <84042050+Inspirate789@users.noreply.github.com>
2022-04-04 17:21:18 +0300

    Adjust the tabs

--- lab_06/input_unsign_oct.asm
+++ lab_06/input_unsign_oct.asm
@@ -3,8 +3,8 @@ PUBLIC Number
 
 DSEG SEGMENT PARA PUBLIC 'DATA'
 	InputMSG            db "Input unsigned octal number: ", '$'
-    NumberErrorMSG      db "Incorrect number. Please, try again.", '$'
-	Number				dw 0                                            ; число занимает 16 бит, старший бит может рассматриваться как знаковый при выводе в знаковом представлении
+    	NumberErrorMSG      db "Incorrect number. Please, try again.", '$'
+	Number		    dw 0                                            ; число занимает 16 бит, старший бит может рассматриваться как знаковый при выводе в знаковом представлении
 	Endline             db 13, 10, '$'
 DSEG ENDS
 
@@ -13,58 +13,58 @@ CSEG2 SEGMENT PARA PUBLIC 'CODE'
 
 input_unsigned_oct proc far
 	mov dx, OFFSET InputMSG
-    mov ah, 09h
-    int 21h                                 ; выводим приглашение к вводу
+    	mov ah, 09h
+    	int 21h                                 ; выводим приглашение к вводу
 
-    mov dx, OFFSET Endline
+    	mov dx, OFFSET Endline
 	int 21h
 
-    xor bx, bx                              ; зануляем регистр, в который мы будем вводить наше число
+    	xor bx, bx                              ; зануляем регистр, в который мы будем вводить наше число
 
-    mov cx, 5                               ; в 16 бит влезет максимум 5 восьмеричных цифр (по 3 бита каждая)
-    mov ah, 01h                             ; выставляем функцию ввода символа
+    	mov cx, 5                               ; в 16 бит влезет максимум 5 восьмеричных цифр (по 3 бита каждая)
+    	mov ah, 01h                             ; выставляем функцию ввода символа
     
-    input_digits_loop:
-        int 21h                             ; вводим символ
+    	input_digits_loop:
+		int 21h                         ; вводим символ
 
-        cmp al, 13
-        je exit_success                     ; нажатие enter является признаком окончания ввода
+		cmp al, 13
+		je exit_success                 ; нажатие enter является признаком окончания ввода
 
-        sub al, '0'                         ; вычитаем '0', чтобы получить из кода символа само число (цифру)
+		sub al, '0'                     ; вычитаем '0', чтобы получить из кода символа само число (цифру)
 
-        cmp al, 7
-        ja exit_failure                     ; если цифра не восьмеричная, выходим, печатая сообщение об ошибке
+		cmp al, 7
+		ja exit_failure                 ; если цифра не восьмеричная, выходим, печатая сообщение об ошибке
 
-        insert_digit:
-        mov dx, cx                          ; сохраняем значение счётчика цикла в регистр dx
-        mov cl, 3
-        shl bx, cl                          ; сдвигаем число на 3 двоичных разряда влево
-        mov cx, dx                          ; восстанавливаем значение счётчика цикла из регистра dx
+		insert_digit:
+		mov dx, cx                      ; сохраняем значение счётчика цикла в регистр dx
+		mov cl, 3
+		shl bx, cl                      ; сдвигаем число на 3 двоичных разряда влево
+		mov cx, dx                      ; восстанавливаем значение счётчика цикла из регистра dx
 
-        add bl, al                          ; помещаем введённную цифру в конец числа
-            
-        loop input_digits_loop
+		add bl, al                      ; помещаем введённную цифру в конец числа
 
-    int 21h                                 ; после 5 цифр ожидаем enter
-    cmp al, 13
-    jne exit_failure
+		loop input_digits_loop
+
+    	int 21h                                 ; после 5 цифр ожидаем enter
+    	cmp al, 13
+    	jne exit_failure
     
-    exit_success:
-    mov Number, bx                          ; помещаем полученное число в Number только при полностью успешном вводе
-    ret
+    	exit_success:
+	mov Number, bx                  	; помещаем полученное число в Number только при полностью успешном вводе
+	ret
     
-    exit_failure:
-    mov dx, OFFSET Endline
+    	exit_failure:
+	mov dx, OFFSET Endline
 	mov ah, 09h
 	int 21h
 
-	mov dx, OFFSET NumberErrorMSG           ; выводим сообщение об ошибке
+	mov dx, OFFSET NumberErrorMSG 		; выводим сообщение об ошибке
 	int 21h
 
-    mov dx, OFFSET Endline
+	mov dx, OFFSET Endline
 	int 21h
-    
-    ret
+
+	ret
      
 input_unsigned_oct endp
 
