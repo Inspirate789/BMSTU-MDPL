6c13a723f870bd21b9d57b387a2c7c959a1f5358
Inspirate789 <84042050+Inspirate789@users.noreply.github.com>
2022-04-04 17:24:45 +0300

    Adjust the tabs

--- lab_06/output_unsign_bin.asm
+++ lab_06/output_unsign_bin.asm
@@ -11,25 +11,25 @@ CSEG3 SEGMENT PARA PUBLIC 'CODE'
 	assume CS:CSEG3, DS:DSEG
 
 output_unsign_bin proc far
-	mov dx, OFFSET OutputBinMSG         ; выводим подсказку к выводу числа
+	mov dx, OFFSET OutputBinMSG         	; выводим подсказку к выводу числа
 	mov ah, 09h
 	int 21h
 
-    mov dx, OFFSET Endline
+    	mov dx, OFFSET Endline
 	int 21h
 
-    mov bx, Number                      ; помещаем наше число в регистр bx
+    	mov bx, Number                      	; помещаем наше число в регистр bx
     
-    mov cx, 16                          ; будем выводить 16 двоичных цифр
-    mov ah, 02h                         ; выставляем функцию вывода символа
+    	mov cx, 16                          	; будем выводить 16 двоичных цифр
+    	mov ah, 02h                         	; выставляем функцию вывода символа
 
-    output_digits_loop:
-        mov dl, 0
+    	output_digits_loop:
+		mov dl, 0
 
-        sal bx, 1                       ; сдвигаем bx на 1 разряд влево через флаг CF
-        adc dl, '0'                     ; в зависимости от флага CF получаем в регистре dl код '0' или '1'
+		sal bx, 1                       ; сдвигаем bx на 1 разряд влево через флаг CF
+		adc dl, '0'                     ; в зависимости от флага CF получаем в регистре dl код '0' или '1'
 
-        int 21h                         ; выводим символ
+		int 21h                         ; выводим символ
 
         loop output_digits_loop
     
@@ -37,7 +37,7 @@ output_unsign_bin proc far
 	mov ah, 09h
 	int 21h
 
-    ret
+    	ret
 output_unsign_bin endp
 
 CSEG3 ENDS
